18721f49b0a1744f89aa661bbd41f87e1f332b4a
Apoorv <apoorvverma0396@gmail.com>
2025-04-02 22:27:42 +0530

    new exes added

---
+++ Python/Exercises/l.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.speed(3)
+turtle.bgcolor('black')
+turtle.pensize(3)
+def func():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+turtle.color('red','pink')
+turtle.begin_fill()
+turtle.left(140)
+turtle.forward(111.5)
+func()
+turtle.left(120)
+func()
